4c3c515046faa1e23e829e394c4093fa9ebc287b
Evgeniy Baranov <master@kompolom.ru>
2016-08-18 10:26:16 +0300

    root commit

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = {
+    /**
+     * Inject size id to image url
+     * @param {String} url
+     * @param {String} sizeId
+     * @returns {string} resized url
+     */
+    resize : function(url, sizeId){
+        var path = url.split('/'),
+            image = path.pop();
+        path.push(sizeId, image);
+        return path.join('/');
+    },
+
+    /**
+     * @deprecated
+     */
+    getResizedUrl : this.resize
+};
